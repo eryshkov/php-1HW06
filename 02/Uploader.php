38df76e2bda28836bec47dbbab7eee25a520a099
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-09 21:34:52 +0300

    Uploader is implemented

--- 02/Uploader.php
+++ 02/Uploader.php
@@ -3,9 +3,37 @@
 class Uploader
 {
     protected $formFieldName;
+    protected $savedImage;
 
     public function __construct($formFieldName)
     {
         $this->formFieldName = $formFieldName;
+    }
+
+    protected function isUploaded(): bool
+    {
+        if (isset($_FILES[$this->formFieldName])) {
+            $savedImage = $_FILES[$this->formFieldName];
+
+            if (0 === $savedImage['error']) {
+                $this->savedImage = $savedImage;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function upload():void
+    {
+        if ($this->isUploaded()) {
+            $savedImagePath = $this->savedImage['tmp_name'];
+            $imageName = $this->savedImage['name'];
+            $imageMimeType = $this->savedImage['type'];
+            $isImage = strpos($imageMimeType, 'image') === 0;
+            if ($isImage) {
+                move_uploaded_file($savedImagePath, __DIR__ . '/img/' . $imageName);
+            }
+        }
     }
 }
