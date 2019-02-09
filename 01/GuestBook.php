eba515bd3b6f521f717a53ab46f2c7a9779aa946
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-09 14:06:17 +0300

    3rd task is completed

--- 01/GuestBook.php
+++ 01/GuestBook.php
@@ -3,20 +3,27 @@
 class GuestBook
 {
     protected $storage;
+    protected $storagePath;
 
     public function getData():array
     {
         return $this->storage;
     }
 
+    public function append($text):void
+    {
+        array_unshift($this->storage, $text);
+    }
+
     public function __construct($fileName)
     {
+        $this->storagePath = $fileName;
         $this->storage = $this->readGuestBook($fileName);
     }
 
     protected function readGuestBook($fileName): array
     {
-        $res = fopen($fileName, 'r');
+        $res = fopen($fileName, 'rb');
         $resultArray = [];
 
         while (false !== $str = fgets($res)) {
