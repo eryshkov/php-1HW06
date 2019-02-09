017d09c9af1469eb16f1386112a79aaee491d57c
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-09 13:34:05 +0300

    first task is completed

---
+++ 01/GuestBook.php
@@ -0,0 +1,27 @@
+<?php
+
+class GuestBook
+{
+    protected $storage;
+
+
+
+    public function __construct($fileName)
+    {
+        $this->storage = $this->readGuestBook($fileName);
+    }
+
+    protected function readGuestBook($fileName): array
+    {
+        $res = fopen($fileName, 'r');
+        $resultArray = [];
+
+        while (false !== $str = fgets($res)) {
+            $resultArray[] = trim($str);
+        }
+
+        fclose($res);
+
+        return $resultArray;
+    }
+}
---
+++ old_guestBook/addRecord.php
@@ -0,0 +1,21 @@
+<?php
+
+include __DIR__ . '/functions.php';
+$filePath = __DIR__ . '/guestBook.txt';
+
+$newRecord = [];
+
+if (isset($_POST['message'])) {
+    $newRecord[] = $_POST['message'];
+}
+
+$prevRecords = readGuestBook($filePath);
+
+$resultArray = array_merge($newRecord, $prevRecords);
+
+$res = fopen($filePath, 'w');
+fwrite($res, implode(PHP_EOL, $resultArray));
+fclose($res);
+
+header('Location:' . '/01/');
+exit;
